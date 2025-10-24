77ad93da431927f184cee4272cc666814530f402
gnmathcopy <1104110@lwsd.org>
2025-10-24 07:36:31 -0700

    Update script.js

--- script.js
+++ script.js
@@ -166,7 +166,7 @@ function displayFeaturedZones(featuredZones) {
     if (featuredContainer.innerHTML === "") {
         featuredContainer.innerHTML = "No featured zones found.";
     } else {
-        document.getElementById("allZonesSummary").textContent = `Hallowen Games (${featuredZones.length})`;
+        document.getElementById("allZonesSummary").textContent = `Spooky Games (${featuredZones.length})`;
     }
 
     const lazyImages = document.querySelectorAll('#featuredZones img.lazy-zone-img');
@@ -710,4 +710,5 @@ document.addEventListener("DOMContentLoaded", () => {
     const randomBtn = document.getElementById("randomZoneBtn");
     randomBtn.addEventListener("click", randomZone);
 });
+
 
